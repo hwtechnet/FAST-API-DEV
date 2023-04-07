9c35497d80476b819bbd88b6175c992809bcc108
Al-Hussein W <130157621+hwtechnet@users.noreply.github.com>
2023-04-07 16:08:42 -0400

    commit

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python
 
-from fastapi import Body, FastAPI
+# https://fastapi.tiangolo.com/tutorial/first-steps/
+
+from fastapi import FastAPI
+from fastapi.params  import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+
+class Post(BaseModel):
+   title: str
+   content: str
+
+
 @app.get("/")
-async def root():
-   return {"message": "Hello World"}
+def root():
+   return {"message": "Welcome to my API!"}
 
 # #Path Parameters
-
+@app.get("/posts")
+def get_posts():
+   return {"data": "This is your posts"}
 
 #Path Parameters with types
+@app.post("/createposts")
+def create_posts(new_posts:Post): # FAST API will reference the class we created earlier and it will check if it has a title or content and make sure they are strings if 
+   # Python will throw an error
+   print(payload)
+   return {"new_post": f"title {payload['title']} content: {payload['content']}"}
 
+# title str, content str
     
